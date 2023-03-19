68d3364840ddda2d527e61af9b5deadf8429f579
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2023-03-19 23:31:27 +0100

    load bookmarks to dom

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,51 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{
+    fs::File,
+    io::{self, BufReader},
+};
+
+use native_dialog::FileDialog;
+use serde::Serialize;
+use tap::Pipe;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    FileDialog(#[from] native_dialog::Error),
+    #[error("no file was chosen using dialog")]
+    NoFileChosen,
+    #[error(transparent)]
+    BookmarkData(#[from] bookmark_data::Error),
+    #[error(transparent)]
+    Io(#[from] io::Error),
+}
+
+impl Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
+}
+
+#[tauri::command]
+async fn open_bookmark_file() -> Result<bookmark_data::Data, Error> {
+    FileDialog::new()
+        .show_open_single_file()?
+        .ok_or(Error::NoFileChosen)?
+        .pipe(File::open)?
+        .pipe(BufReader::new)
+        .pipe(bookmark_data::Data::load)?
+        .pipe(Ok)
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![open_bookmark_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,49 @@
 import init, { run_on_load } from "src-wasm";
+import { invoke } from "@tauri-apps/api/tauri"
+
+interface Bookmark {
+	info: string
+	url: string
+	uuid: string
+	tag: Array<string>
+}
+
+interface Category {
+	info: string
+	name: string
+	subcategory: Array<Category>
+}
+
+interface LoadedData {
+	bookmark: Array<Bookmark>
+	Category: Array<Category>
+	tag: Array<string>
+}
 
 init().then(() => {
-	run_on_load();
-});
+	run_on_load()
+})
+
+document.querySelector("#open-btn")?.addEventListener("click", () => {
+	invoke("open_bookmark_file").then(loaded => {
+		const content = <LoadedData>loaded
+		const list = <HTMLUListElement>document.querySelector("ul#bookmark-list")
+
+		console.log(content)
+		for (let bookmark of content.bookmark) {
+			const link = <HTMLAnchorElement>document.createElement("a")
+			link.textContent = bookmark.url;
+			link.href = bookmark.url
+
+			const info = <HTMLParagraphElement>document.createElement("p")
+			info.textContent = bookmark.info;
+
+			const row = <HTMLLIElement>document.createElement("li")
+			row.append(info, link)
+
+			list.append(row)
+		}
+	}).catch(e => {
+		console.log(e)
+	})
+})
